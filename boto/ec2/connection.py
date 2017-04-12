39419c24062958f62ac27a6f418c75f53d5f6395
Anton Vazhnetsov <dragen15051@gmail.com>
2017-04-12 14:27:42 +0300

    ec2: fix volumes attach/detach methods according to AWS API
    http://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_AttachVolume.html#API_AttachVolume_ResponseElements
    
    Signed-off-by: Anton Vazhnetsov <dragen15051@gmail.com>

--- boto/ec2/connection.py
+++ boto/ec2/connection.py
@@ -41,6 +41,7 @@ from boto.ec2.instance import ConsoleOutput, InstanceAttribute
 from boto.ec2.keypair import KeyPair
 from boto.ec2.address import Address
 from boto.ec2.volume import Volume, VolumeAttribute
+from boto.ec2.volume import AttachmentSet
 from boto.ec2.snapshot import Snapshot
 from boto.ec2.snapshot import SnapshotAttribute
 from boto.ec2.zone import Zone
@@ -2424,6 +2425,10 @@ class EC2Connection(AWSQueryConnection):
 
         :rtype: bool
         :return: True if successful
+
+        :rtype: class: `boto.ec2.volume.AttachmentSet`
+        :return: Attribute status of returned object will be 'attached'
+                 in case of success
         """
         params = {'InstanceId': instance_id,
                   'VolumeId': volume_id}
@@ -2433,7 +2438,7 @@ class EC2Connection(AWSQueryConnection):
             params['Device'] = device
         if attach_type is not None:
             params['AttachType'] = attach_type
-        return self.get_status('AttachVolume', params, verb='POST')
+        return self.get_object('AttachVolume', params, AttachmentSet, verb='POST')
 
     def detach_volume(self, volume_id, instance_id=None,
                       device=None, force=False, dry_run=False):
@@ -2464,8 +2469,9 @@ class EC2Connection(AWSQueryConnection):
         :type dry_run: bool
         :param dry_run: Set to True if the operation should not actually run.
 
-        :rtype: bool
-        :return: True if successful
+        :rtype: class: `boto.ec2.volume.AttachmentSet`
+        :return: Attribute status of returned object will be 'detached'
+                 in case of success
         """
         params = {'VolumeId': volume_id}
         if instance_id:
@@ -2476,7 +2482,7 @@ class EC2Connection(AWSQueryConnection):
             params['Force'] = 'true'
         if dry_run:
             params['DryRun'] = 'true'
-        return self.get_status('DetachVolume', params, verb='POST')
+        return self.get_object('DetachVolume', params, AttachmentSet, verb='POST')
 
     # Snapshot methods
 
